44bddabcf97a4c309a0bc97cc04d7835097c8f4c
Joevenable <binechorighteous@gmail.com>
2021-02-22 17:17:16 -0600

    i completed a few unit tests i will create more

---
+++ CustListTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustListTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustListTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4445b8e2-24ef-4b1d-a41c-c06994c60de5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustListTest/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustList;
+
+namespace CustListTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_EmptyUnitListStartingCapacity()
+        {
+            //arrange
+            CustomList<string> customListStrings = new CustomList<string>();
+
+            int expected = 4;
+            int actual;
+
+            //act
+
+            int actual = customListStrings.Capacity;
+
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Add_Item_CountIncreasesByOne()
+        {
+            //arrange
+            CustomList<string> customListStrings = new CustomList<string>();
+
+            
+            int expected = 5;
+            int actual;
+
+            //act
+
+            customListStrings.Add("Tree");
+            customListStrings.Add("Bush");
+            customListStrings.Add("Shrub");
+            customListStrings.Add("Grape");
+            customListStrings.Add("Leaf");
+            int actual = customListStrings.Count;
+
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_FiveItemUnitListStartingCapacity()
+        {
+            //arrange
+            CustomList<string> customListStrings = new CustomList<string>();
+
+            int expected = 8;
+            int actual;
+
+            //act
+            customListStrings.Add("Tree");
+            customListStrings.Add("Bush");
+            customListStrings.Add("Shrub");
+            customListStrings.Add("Grape");
+            customListStrings.Add("Leaf");
+            int actual = customListStrings.Capacity;
+
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
